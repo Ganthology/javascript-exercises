29f0a0e75504ff1b2b0c28cf35d431d917a20261
Boon Kit <gbk1386@gmail.com>
2020-12-03 17:30:41 +0800

    Add solution for reverseString.js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Split the string to array
+    let letters = string.split("");
+    let length = letters.length;
+    // Let a new variable
+    let newString = [];
+    // Use for loop to reverse
+    for (let i = 0; i < length; i++) {
+        newString[i] = letters[length - 1 - i];
+    }
+    return newString.join("");
 }
 
 module.exports = reverseString
