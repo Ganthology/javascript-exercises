966b3d1f0278e290b81c0f8b51945e6030972c56
Boon Kit <gbk1386@gmail.com>
2020-12-03 17:25:40 +0800

    Save

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,7 +1,13 @@
 const palindromes = function(string) {
-    string = string.replace(/[\W_]/g,"").toLowerCase();
+    // \W match any non-word character [^a-zA-Z0-9_]
+    // The regex will keep the string only letters and numbers
+    // The letters are toLowerCase() for the ease of comparison
+    string = string.replace(/[\W_]/g,"").toLowerCase(); 
+    // The string are split into array of chars
     let splitStr = string.split("");
+    // The array of chars are reversed then joined again to string
     let palindrome = splitStr.reverse().join("");
+    // Compare the input and reversed string
     return (string === palindrome);
 }
 
