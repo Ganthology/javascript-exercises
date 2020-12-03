4c2d107fe8654c18fbd03b94109e6a180741c328
Boon Kit <gbk1386@gmail.com>
2020-12-03 17:25:57 +0800

    Add solution for repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat = Math.floor(Math.random() * 1000)) {
+    // set default times to random
+    let result = '';
+    // return ERROR if repeat is negative
+    if (repeat < 0) return 'ERROR';
+    // use for loop to repeat
+    for (let i = 0; i < repeat; i++) {
+        result += string;
+    }
+    return result;
 }
 
 module.exports = repeatString
