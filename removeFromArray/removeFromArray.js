a4f78ab6af15deb02e659bf2749da5ad5e49f52b
Boon Kit <gbk1386@gmail.com>
2020-12-03 17:18:35 +0800

    Add solution for removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    // remove one specified value
+    // remove more than one specific value
+    // if not exist then ignore
+    // remove strings
+    let args = Array.prototype.slice.call(arguments, 1);
+    for (let i of args){
+        let index = array.indexOf(i);
+        if (index > -1) array.splice(index, 1);
+    }
+    return array;
 }
 
 module.exports = removeFromArray
