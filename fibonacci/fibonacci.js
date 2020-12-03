78d26912e580e62e739855c0a293f83b9dc31f35
Boon Kit <gbk1386@gmail.com>
2020-12-03 17:20:53 +0800

    Add solution for fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    // Unary + makes the input becomes number type
+    number = +number;
+    if (number <= 0) return "OOPS";
+    else if (number == 1) return 1;
+    else if (number == 2) return 1;
+    else return fibonacci(number-1)+fibonacci(number-2);
 }
 
 module.exports = fibonacci
