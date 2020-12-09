1dd922fd431597320715fcb2daaf20d3bf1ff5e1
Ganthology <gbk1386@gmail.com>
2020-12-10 00:26:11 +0800

    Add solution

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,32 @@
-function add () {
-	
+function add (number1, number2) {
+	return number1 + number2;
 }
 
-function subtract () {
-	
+function subtract (number1, number2) {
+	return number1 - number2;
 }
 
-function sum () {
-	
+function sum (array) {
+	return array.reduce((accumulator, item)=>{
+		return accumulator += item;
+	}, 0);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	return array.reduce((accumulator, item)=>{
+		return accumulator *= item;
+	}, 1);
 }
 
-function power() {
-	
+function power(number, power) {
+	return number**power;
 }
 
-function factorial() {
-	
+function factorial(number) {
+	if(number == 0) return 1;
+	const range = (number1, number2) => Array.from({length: Math.abs(number1 - number2) + 1},(_,i) => (Math.min(number1,number2) + i));
+    let array = range(1, number);
+    return array.reduce((accumulator, array) => accumulator * array, 1);
 }
 
 module.exports = {
